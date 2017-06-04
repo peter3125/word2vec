937a4ae7719865bf536de0c96d0a43f0f33ead79
Rock de Vocht <peter@peter.co.nz>
2017-06-05 09:08:54 +1200

    added new test script

---
+++ scripts/semantic-net.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DATA_DIR=../data
+BIN_DIR=../bin
+SRC_DIR=../src
+
+TEXT_DATA=$DATA_DIR/wn2vec.txt
+VECTOR_DATA=$DATA_DIR/wordnet_semantic_vecs.bin
+
+pushd ${SRC_DIR} && make; popd
+
+if [ ! -e $VECTOR_DATA ]; then
+  echo -----------------------------------------------------------------------------------------------------
+  echo -- Training vectors...
+  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 3 -window 3 -negative 0 -hs 1 -sample 1e-3 -threads 20 -binary 0
+
+fi
+
+echo -----------------------------------------------------------------------------------------------------
+echo -- distance...
+
+$BIN_DIR/distance $DATA_DIR/$VECTOR_DATA
